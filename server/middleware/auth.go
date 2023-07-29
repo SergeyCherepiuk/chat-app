563a2f07aeecb70f6d6cc617f82f399ff4bde83f
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-07-29 21:56:55 +0200

    Log auth middleware

--- server/middleware/auth.go
+++ server/middleware/auth.go
@@ -1,9 +1,11 @@
 package middleware
 
 import (
+	"github.com/SergeyCherepiuk/chat-app/logger"
 	authstorage "github.com/SergeyCherepiuk/chat-app/storage/auth"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
+	"golang.org/x/exp/slog"
 )
 
 type AuthMiddleware struct {
@@ -18,11 +20,21 @@ func (middleware AuthMiddleware) CheckIfAuthenticated() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		sessionId, err := uuid.Parse(c.Cookies("session_id", ""))
 		if err != nil {
+			logger.Logger.Error(
+				"failed to parse UUID",
+				slog.String("err", err.Error()),
+				slog.String("session_id", c.Cookies("session_id", "")),
+			)
 			return c.SendStatus(fiber.StatusUnauthorized)
 		}
 
 		userId, err := middleware.storage.Check(sessionId)
 		if err != nil {
+			logger.Logger.Error(
+				"failed to find a session",
+				slog.String("err", err.Error()),
+				slog.Any("session_id", sessionId),
+			)
 			return c.SendStatus(fiber.StatusUnauthorized)
 		}
 
