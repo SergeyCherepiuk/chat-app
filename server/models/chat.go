a4e51c6db1ac73b13493998aad6d489c11ceaaed
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-07-30 16:34:04 +0200

    Change models Add gorm "not null" constraints Set messages' sender equal to null when user deletes an account

--- server/models/chat.go
+++ server/models/chat.go
@@ -2,6 +2,6 @@ package models
 
 type Chat struct {
 	ID       uint      `json:"id" db:"id" gorm:"primary"`
-	Name     string    `json:"name" db:"name"`
+	Name     string    `json:"name" db:"name" gorm:"not null"`
 	Messages []Message `json:"-" gorm:"constraint:OnDelete:CASCADE;"`
 }
--- server/models/message.go
+++ server/models/message.go
@@ -4,8 +4,8 @@ import "time"
 
 type Message struct {
 	ID     uint      `json:"id" db:"id" gorm:"primary"`
-	Text   string    `json:"text" db:"text"`
-	SentAt time.Time `json:"sent_at" db:"send_at"`
+	Text   string    `json:"text" db:"text" gorm:"not null"`
+	SentAt time.Time `json:"sent_at" db:"send_at" gorm:"not null"`
 	UserID uint      `json:"user_id" db:"user_id"`
-	ChatID uint      `json:"chat_id" db:"chat_id"`
+	ChatID uint      `json:"chat_id" db:"chat_id" gorm:"not null"`
 }
